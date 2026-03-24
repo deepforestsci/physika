8924de8094dc934b289a74613cc8a83fa02778fb
a-b-h-a-y-s-h-i-n-d-e <theabhay.shinde@gmail.com>
2026-03-25 03:59:46 +0530

    updated physika file with correct torch and ast code

--- examples/torch_code/example_check_gradients.py
+++ examples/torch_code/example_check_gradients.py
@@ -7,10 +7,10 @@ from physika.runtime import compute_grad
 
 # === Functions ===
 def f(x):
-    y = ((x[int(0.0)] ** 2.0) + (x[int(1.0)] ** 2.0))
+    y = (x[int(0.0)] ** 2.0)
     g = compute_grad(y, x)
     return g
 
 # === Program ===
-v = torch.tensor([1.0, 2.0], requires_grad=True)
+v = torch.tensor([1.0], requires_grad=True)
 physika_print(f(v))
