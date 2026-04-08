1802604d0b180bf0fa0efba3e0da84b5477e8b6f
a-b-h-a-y-s-h-i-n-d-e <theabhay.shinde@gmail.com>
2026-04-08 12:14:43 +0530

    added basic expression level boolean operator support

---
+++ examples/ast/example_boolean_operator.py
@@ -0,0 +1,6 @@
+EXPECTED = {'functions': {},
+ 'classes': {},
+ 'program': [('decl', 'a', 'ℝ', ('num', 1), 1),
+             ('decl', 'b', 'ℝ', ('num', 0), 2),
+             ('expr', ('or', ('var', 'a'), ('var', 'b')), 0),
+             ('expr', ('and', ('var', 'a'), ('var', 'b')), 0)]}
