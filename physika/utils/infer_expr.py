754ad13ccd0bf3f0272807a3ba94d03bc0282c2f
a-b-h-a-y-s-h-i-n-d-e <theabhay.shinde@gmail.com>
2026-06-17 12:48:06 +0530

    added infer_expr to include class methods

--- physika/utils/infer_expr.py
+++ physika/utils/infer_expr.py
@@ -1079,7 +1079,20 @@ def expr_call(node: Any,
                     resolved_dims.append((dim, variance))
             ret = TTensor(tuple(resolved_dims))
         return ret, s
+    
+    if func_name == "this":
+        this_type = ctx.env.get("this")
 
+        if isinstance(this_type, TInstance):
+            class_info = ctx.class_env.get(this_type.class_name)
+            if class_info is not None:
+                methods = class_info.get("methods", {})
+
+                if "λ" in methods:
+                    lambda_info = methods["λ"]
+
+                    ret_type = from_typespec(lambda_info.get("return_type"))
+                    return ret_type, s
     # Unknown call
     return None, s
 
