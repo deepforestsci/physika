2d7d7d107dd248ee34424cba2ad6d1e896b74864
Iammohithhh <iammohith2004@gmail.com>
2026-07-20 21:12:55 +0530

    Minor changes in the comments and removed VWN reference in XC

--- examples/torch_code/dft_potentials.py
+++ examples/torch_code/dft_potentials.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from physika.runtime import DEVICE
 
 from physika.runtime import physika_print
 
@@ -15,11 +16,11 @@ def coulomb(atoms):
 def flat_index(k):
     return torch.arange(k)
 
-def axis_index(ms, divisor, modulus):
-    return torch.remainder(torch.floor((ms / divisor) if isinstance((ms / divisor), torch.Tensor) else torch.tensor(float((ms / divisor)))), modulus)
+def axis_index(ms, axis_stride, axis_length):
+    return torch.remainder(torch.floor((ms / axis_stride) if isinstance((ms / axis_stride), torch.Tensor) else torch.tensor(float((ms / axis_stride)))), axis_length)
 
-def fold_freq(m, q):
-    return (m - (torch.gt(m, (q / 2)) * q))
+def fold_freq(m, axis_length):
+    return (m - (torch.gt(m, (axis_length / 2)) * axis_length))
 
 def cell_volume(a):
     return ((a * a) * a)
@@ -39,15 +40,18 @@ def active_mask(G2, ecut):
 def active_subset(G2, active):
     return torch.masked_select(G2, active)
 
-def structure_factor(n1, n2, n3, cc, px, py, pz):
-    phase = (cc * (((n1 * px) + (n2 * py)) + (n3 * pz)))
+def structure_factor(n1, n2, n3, c, px, py, pz):
+    phase = (c * (((n1 * px) + (n2 * py)) + (n3 * pz)))
     return torch.exp(((-torch.tensor(1j)) * phase) if isinstance(((-torch.tensor(1j)) * phase), torch.Tensor) else torch.tensor(float(((-torch.tensor(1j)) * phase))))
 
-def op_J(W, s1, s2, s3):
-    n = ((s1 * s2) * s3)
-    cube = torch.reshape(W, (int(s1), int(s2), int(s3),))
-    spec = torch.fft.fftn(cube)
-    return (torch.reshape(spec, (int(n),)) / n)
+def op_J(atoms, W):
+    s1 = atoms.s1
+    s2 = atoms.s2
+    s3 = atoms.s3
+    Ngrid = ((s1 * s2) * s3)
+    real_grid = torch.reshape(W, (int(s1), int(s2), int(s3),))
+    reciprocal_grid = torch.fft.fftn(real_grid)
+    return (torch.reshape(reciprocal_grid, (int(Ngrid),)) / Ngrid)
 
 # === Classes ===
 class Atoms(nn.Module):
